5af065d33ff4382e0c4d3be6295b7c3cadd79a28
Efrain <efraincruzlobato13@gmail.com>
2022-02-25 11:14:05 -0600

    fech Api

---
+++ src/components/GifGrid.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+export const GifGrid = ({ category }) => {
+
+    const getGifs = async() => {
+
+        const url = 'https//api.giphy.com/v1/gifs/search?q=Rick+and+Morty&limi=106api_key=A8xMXqziI'
+        const resp = await fetch( url );
+        const { data } = await resp.json();
+
+        const gifs = data.map( img => {
+            return {
+                id: img.id,
+                title: img.title,
+                url: img.images?.downssized_medium.url
+            }
+        })
+
+        console.log(gifs);
+    }
+
+    getGifs();
+
+    return {
+        
+    }
+
+
+    
+}
