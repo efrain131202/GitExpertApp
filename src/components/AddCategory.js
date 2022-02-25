2ddddcef7ff506c4692e0a3a558d7a743e64e18e
Efrain <efraincruzlobato13@gmail.com>
2022-02-25 10:53:50 -0600

    Comunicacion entre componentes

--- src/components/AddCategory.js
+++ src/components/AddCategory.js
@@ -12,7 +12,10 @@ export const AddCategory = ({ setCategories }) => {
     const handleSubmit = (e) => {
         e.preventDefault();
 
-        
+        if ( inputValue.trim().length > 2 ) {
+            setCategories( cats => [ inputValue, ...cats, ] );
+            setInputValue('');
+        }
 
     }
 
