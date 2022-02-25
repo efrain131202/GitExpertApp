6ed7e4f3c7cb0e2f7d2c3b1c97eef6a399f8847a
Efrain <efraincruzlobato13@gmail.com>
2022-02-25 12:37:15 -0600

    UseFetchGifs

--- src/hooks/useFetchGifs.js
+++ src/hooks/useFetchGifs.js
@@ -1,22 +1,26 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import { getGifs } from '../helpers/getGifs';
 
 
-
-export const useFetchGifs = ( ) => {
+export const useFetchGifs = ( category ) => {
     
     const [state, setState] = useState({
         data: [],
         loading: true
     });
 
-    setTimeout{ () => {
-        setState({ 
-        data: [1,2,3,4,5,6,7],
-        loading: false
+    useEffect( () => {
 
-        })
-    }, 3000 );
+        getGifs( category )
+            .then( imgs => {
+                
+                setState({
+                    data: imgs,
+                    loading: false
+                });
+            })
 
+    }, 3000 )
 
 
 
